db9ef9f81ba06ff621039ddcf86ecb11251dde26
Emran Hossain <emranhossain0335@gmail.com>
2024-01-13 16:23:48 +0600

    gitlearning file is created and it contain the information of git commands and their explanation

---
+++ gitLearning.js
@@ -0,0 +1,45 @@
+/*
+Git is a version control system/tool that allows you to track changes to your code over time. 
+1. What is verson control?
+Ans: Version control is the process of tracking changes to your code of the file or folder's file over time. It allows you to revert back to previous versions of your code if necessary. For example, if we write somethig in a file and leave it to anybody, another person will aslo write something in this file. Then the   version of will be changed and it will get second version as something is write on the file. So we can revert back to the first version of the file if we need to. 
+2. What is git?
+Ans: Git is a version control system that allows you to track changes to your code over time. It is a popular tool for version control in software development. It allows you to store and manage changes to your code over time. 
+3. What is the difference between git and github?
+Ans: Git is a version control system that allows you to track changes to your code over time. It is a popular tool for version control in software development. It allows you to store and manage changes to your code over time. 
+Github is a cloud-based platform that allows you to store and manage your code on the cloud. It is a popular tool for version control in software development. It allows you to store and manage changes to your code over time. 
+4. What is commit?
+Ans: Commit is a record of changes made to your code. It is used to take control of the file or folder to git after tracking/staging.
+*/
+/*
+1. .git 
+Ans: When something is created in a folder with dot(.git). It is then called a hidden folder. This hidden(.git folder) can track everything on the local repository folder.
+2. ls -a
+Ans: It means to show all listed files even with all hidden files.
+*/
+
+/*
+1. git config --list
+Ans: This command is used to list all the configuration settings for git. 
+2. git config --global user.name "Emran Hossain"
+Ans: This command is used to set the name of the user for git. 
+3. git config --global user.email "emranhossain0335@gmail.com"
+Ans: This command is used to set the email address of the user for git. 
+*/
+
+/*
+** important conmmands **
+1. touch readme.md
+Ans: This command is used to create a new file. 
+2. git init
+Ans: This command is used to initialize a git repository. 
+3. git add readme.md
+Ans: This command is used to stage/track a specific file for commit. It is used to track a file so that git can control the version of the file.
+4. git add --all
+Ans: This command is used to stage or track all the files and folders.
+5. git add .
+Ans: This command is used to stage or track the current directory or file. But if we use it in the root folder, then all files or folder or sub-folder will be staged or tracked.
+6. git status
+Ans: This command is used to check the status of the git repository. It shows the current state of the repository. It shows the status of the files and folders. It shows the staged files and folders(new files and folder). It also shows the unstaged/untracked files and folders.
+7. git commit -m "first commit"
+Ans: This command is used to commit/save the staged files and folders. It is used to take control of the file or folder by git after tracking/staging. 
+*/
