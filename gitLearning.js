436ee8852c5b49c687bb5fcb56314bfc9356dad7
Emran Hossain <emranhossain0335@gmail.com>
2024-01-14 08:28:39 +0600

    branching commands are going to be learned

--- gitLearning.js
+++ gitLearning.js
@@ -64,6 +64,14 @@ Ans: This command is used to reset the file to the specified commit. It is used 
 15. git reflog
 Ans: This command is used to show the reflog(all the history of commits) of the git repository. It shows the history of the commits made to the files and folders.
 16. git rm file's name(help.md)
-Ans: This command is used to remove the file from the git repository. This particular file is removed from the working directory but git still does not know that the file is removed from its local directory. So, commit is needed in this situation.
+Ans: This command is used to remove the file from the git repository. This particular file is removed from the working directory but git still does not know that the file is removed from its local directory. So, commit is needed in this situation. But we can move to the previos commits(using commitId) and there we will find the deleted file.
+*/
+
+/*
+** Branching **
+1. git branch --list
+Ans: This command is used to show the list of all the branches.
+2. git branch branchName(feature/dashboard)
+Ans: This command is used to create a new branch. But we have to follow rules to name the branch such as bugfix/readme, feature/dashboard, etc. Branches will always be created from thier parent branch.
 
 */
